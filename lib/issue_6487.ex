5f15778526b67206e03ab092861ce56935c1b3f6
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2017-08-22 21:44:46 +0200

    Add Foo and Foo.Bar
    ... as per https://github.com/elixir-lang/elixir/issues/6487#issue-251497125

--- lib/issue_6487.ex
+++ lib/issue_6487.ex
@@ -1,18 +1,19 @@
-defmodule Issue6487 do
-  @moduledoc """
-  Documentation for Issue6487.
-  """
+defmodule Foo do
+  defmodule Bar do
+    @moduledoc false
+
+    def hello(name, _options) do
+      IO.puts "Hello, #{name}"
+    end
+  end
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Issue6487.hello
-      :world
-
+  Prints a welcoming message
   """
-  def hello do
-    :world
-  end
+  defdelegate hello(name, options \\ []), to: __MODULE__.Bar
+
+  @doc """
+  Prints hi
+  """
+  def hi(name, options \\ []), do: IO.puts "Hi!"
 end
